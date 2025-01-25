49316f05c1823c0cc443f1886ac10f19bc7211fa
SarmisthaMoharer <moharersarmistha3@gmail.com>
2025-01-25 21:23:15 +0530

    first commit

---
+++ src/utils/Themes.js
@@ -0,0 +1,44 @@
+/*export const darkTheme = {
+  bg: "#090917",
+  bgLight: "#1C1E27",
+  primary: "#854CE6",
+  text_primary: "#F2F3F4",
+  text_secondary: "#b1b2b3",
+  card: "#171721",
+  card_light: "#191924",
+  button: "#854CE6",
+  white: "#FFFFFF",
+  black: "#000000",
+};
+
+export const lightTheme = {
+  bg: "#FFFFFF",
+  bgLight: "#f0f0f0",
+  primary: "#be1adb",
+  text_primary: "#111111",
+  text_secondary: "#48494a",
+  card: "#FFFFFF",
+  button: "#5c5b5b",
+};*/
+export const darkTheme = {
+  bg: "#1c2727",
+  bgLight: "#1C1E27",
+  primary:"#4cd1e6",
+  text_primary:"#F2F3F4",
+  text_secondary:"#b1b2b3",
+  card:"#171721",
+  card_light: '#191924',
+  button:"#e64c94",
+  white:"#FFFFFF",
+  black:"#000000",
+}
+
+export const lightTheme = {
+  bg:"#FFFFFF",
+  bgLight: "#f0f0f0",
+  primary:"#1a9edb",
+  text_primary:"#111111",
+  text_secondary:"#48494a",
+  card:"#FFFFFF",
+  button:"#5c5b5b",
+}
